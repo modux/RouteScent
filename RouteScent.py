658eca3803458c057af000fe384217daf79dfc8a
modux <sash@modux.co.uk>
2014-08-16 06:19:43 -0400

    Rename file

--- RouteScent.py
+++ RouteScent.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 '''
 things need to grab automatically
 
@@ -18,73 +20,129 @@ igmpv2 - gw sends out general query
 detect arp packets - responses
 
 compare with udp/tcp/icmp packets containing the same mac
-
-gwmac = ""
-gwip = ""
-iptable = {}
-arptable = {}
 '''
+from scapy.all import *
+import argparse
 
-def dhcp_pkt(pkt):
-	global iptable, arptable, gwmac, gwip
-	gwip = pkt.gwip
-	
-	print "Gateway IP " + gwip + "found through DHCP response"
-	
-	try:
-		gwmac = iptable[gwip]
-		
-		print "Gateway MAC " + gwmac + "found through DHCP "
-		
-	except:
-		pass
+class RouteScent():
+  
+  def __init__(self):
+  
+    self.gwmac = ""
+    self.gwip = ""
+    self.iptable = {}
+    self.arptable = {}
+    self.ttls=[64,128,32,60,255]  
+  
+  def dhcp_pkt(self,pkt):
+    
+        
+    print "Gateway IP " + gwip + "found through DHCP response"
+    
+    try:
+      gwmac = iptable[gwip]
+      
+      print "Gateway MAC " + gwmac + "found through DHCP "
+      
+    except:
+      pass
+  
+    
+  def ip_pkt(self,pkt):
+    
+    
+    self.iptable[pkt[IP].src] = pkt.src
+    self.iptable[pkt[IP].dst] = pkt.dst
+    
+    self.arptable[pkt.src]=pkt[IP].src
+    self.arptable[pkt.dst]=pkt[IP].dst
+    # search IP tables for multiple entries of same MAC
+    print pkt.ttl     
+    for ip, mac in self.iptable.items():
+      if ((mac == pkt.src) and (ip != pkt[IP].src)):
+        self.gwmac = mac
+        print "GW MAC!!" + self.gwmac
+        if pkt.ttl in self.ttls:
+          print "GW IP" + pkt[IP].src
+      if ((mac == pkt.dst) and (ip != pkt[IP].dst)):
+        self.gwmac = mac
+        print "GW MAC!!" + self.gwmac
+        
+    
+    if pkt.src in self.arptable and \
+       pkt[IP].src != self.arptable[pkt.src]:
+        self.gwip = self.arptable[pkt.src]
+        print self.gwip
+    if pkt.dst in self.arptable and \
+       pkt[IP].dst != self.arptable[pkt.dst]:
+          self.gwip = self.arptable[pkt.dst]
+    if self.gwip is not "":
+      print "GW found through analysis!" + self.gwip
+  
+  def arp_pkt(self,pkt):
+      
+    
+    self.arptable[pkt.srcmac] = pkt.srcip
+    self.iptable[pkt.srcip] = pkt.srcmac
+  
+  def process_pkt(self,pkt):
+  
+    if pkt.haslayer(DHCP):
 
-	
-def ip_pkt(pkt):
-	global iptable, arptable, gwmac, gwip
-	iptable[pkt.srcip] = pkt.srcmac
-	iptable[pkt.dstip] = pkt.dstmac
+      self.dhcp_pkt(pkt)
+      
+    elif pkt.haslayer(IP):    
+      self.ip_pkt(pkt)
+      
+    #elif pkt.type== ip:
+  
+     # ip_pkt(pkt)
+      
+    #elif pkt.type==igmp:
+  
+    #  igmp_pkt(pkt)
+      
+  
+  def threaded_sniff_target(self):
+           
+    conf.iface=self.iface
+          
+    sniff(prn = lambda x : self.process_pkt(x))
+ 
+  def read_file(self,file):
 
-	# search IP tables for multiple entries of same MAC
-	
-	for ip, mac in iptable:
-		if ((mac == pkt.srcmac) and (ip != pkt.srcip)) or \
-			((mac == pkt.dstmac) and (ip != pkt.dstip)):
-			gwmac = mac
-			
-		
-	if pkt.srcmac in arp_tables:
-		if pkt.srcip != arp_table[pkt.srcmac]:
-			gwip = arp+table[pkt.srcmac]
+    pkts=rdpcap(file)
+    for p in pkts:
+      self.process_pkt(p)
+       
+def main():
+  parser = argparse.ArgumentParser(description="Network SED for on the wire find and replace", usage="./RouteScent.py [-i eth0] [-f filename] [options]")
 
-	if pkt.dstmac in arp_tables:
-			if pkt.dstip != arp_table[pkt.dstmac]:
-				gwip = arp+table[pkt.dstmac]
-	
 
-def arp_pkt(pkt):
-	global iptable, arptable, gwmac, gwip	
-	
-	arptable[pkt.srcmac] = pkt.srcip
-	iptable[pkt.srcip] = pkt.srcmac
+  parser.add_argument('-i',"--interface",dest="iface", metavar='REGEX',type=str,help="Interface to smell on")
+  parser.add_argument('-f', '--file',metavar='Pcap',type=str,help='PCAP file to read')
+  
+  args=parser.parse_args()
+  
 
-	def pkt_process(pkt)
+  rs=RouteScent()
+  
+  if args.iface:
+    
+    rs.iface=args.iface
+    rs.threaded_sniff_target()
+  elif args.file:
+    
+    rs.read_file(args.file)  
 
-	if pkt.type() == dhcp:
+  while 1:
+    pass
+#  while (gwip == "") and (gwmac == ""):
+ #   print
 
-		dhcp_pkt(pkt)
-		
-	else if:
-		
-		arp_pkt(pkt)
-		
-	else if:
 
-		ip_pkt(pkt)
-		
-	else if:
 
-		igmp_pkt(pkt)
-		
+if __name__=="__main__":
+    main()
 
-while (gwip == "") and (gwmac == "")
+
